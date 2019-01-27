5f524c8cb4b23cb5fbe6a1115e8cf5de27139109
MarekUlip <marek.ulip@gmail.com>
2019-01-27 23:25:16 +0100

    Removed coroutines - bad performance. Code cleanup.

--- src/main/kotlin/GameOfLife.kt
+++ src/main/kotlin/GameOfLife.kt
@@ -1,19 +1,17 @@
-import com.sun.org.apache.xpath.internal.operations.Bool
 import javafx.scene.paint.Color
 import javafx.scene.shape.Rectangle
 import kotlin.random.Random
-import kotlinx.coroutines.*
 import java.util.concurrent.atomic.AtomicInteger
 
 class GameOfLife(val width: Int, val height: Int, val output: MutableList<MutableList<Rectangle>>?) {
 
-    val empty = 0
-    val full = 1
-    var matrix = mutableListOf<MutableList<Int>>()
+    private val empty = 0
+    private val full = 1
+    private var matrix = mutableListOf<MutableList<Int>>()
 
     fun generateMatrix(){
         for (i in 0..height){
-            matrix.add(mutableListOf<Int>())
+            matrix.add(mutableListOf())
             for (j in 0..width){
                 val rnd = Random.nextInt()
                 if (rnd > 0.5) {
@@ -25,14 +23,14 @@ class GameOfLife(val width: Int, val height: Int, val output: MutableList<Mutabl
         }
     }
 
-    fun isOutOfBounds(value: Int, maximum: Int): Boolean{
+    private fun isOutOfBounds(value: Int, maximum: Int): Boolean{
         if(value<0 || value>=maximum){
             return true
         }
         return false
     }
 
-    fun getNewCellState(cell: Int, neighCount: Int): Int{
+    private fun getNewCellState(cell: Int, neighCount: Int): Int{
         if(cell == empty){
             if (neighCount == 3){
                 return full
@@ -74,7 +72,6 @@ class GameOfLife(val width: Int, val height: Int, val output: MutableList<Mutabl
                             neighCount += matrix[i + x][j + y]
                         }
                     }
-                    //println(neighCount)
                     newMatrix[index][jIndex] = getNewCellState(matrix[index][jIndex], neighCount)
                 }
             }
@@ -98,10 +95,9 @@ class GameOfLife(val width: Int, val height: Int, val output: MutableList<Mutabl
                 val newMatrix = matrix.toMutableList()
 
                     repeat(threadNum) {
-                        //launch {
                         val chunkNum = it
                         threads.add(Thread {
-                            val subIndex = (height / threadNum).toInt()///index.getAndIncrement()
+                            val subIndex = (height / threadNum).toInt()
                             val end = if (chunkNum == threadNum - 1) {
                                 matrix.size - 1
                             } else {
@@ -128,10 +124,8 @@ class GameOfLife(val width: Int, val height: Int, val output: MutableList<Mutabl
                                             neighCount += matrix[i + x][j + y]
                                         }
                                     }
-                                    //println(neighCount)
                                     newMatrix[baseI][baseJ] = getNewCellState(matrix[baseI][baseJ], neighCount)
                                 }
-                                //}
                             }
                         })
                         threads[chunkNum].start()
@@ -145,180 +139,7 @@ class GameOfLife(val width: Int, val height: Int, val output: MutableList<Mutabl
             }while (infinity)
     }
 
-
-    fun runGameOfLifeConstrained(infinity: Boolean) {
-        //while (true) {
-        //val coroutines = mutableListOf<Job>()
-        var start = System.currentTimeMillis()
-        val index = AtomicInteger(0)
-        val jIndex = AtomicInteger(0)
-        val threadNum = 8
-        runBlocking {
-            do {
-                start = System.currentTimeMillis()
-                index.set(0)
-                val newMatrix = matrix.toMutableList()
-                coroutineScope {
-                    repeat(threadNum) {
-                        //launch {
-                        val chunkNum = it
-                        val subIndex = (height / threadNum).toInt()///index.getAndIncrement()
-                        val end = if (chunkNum == threadNum-1){
-                            matrix.size-1
-                        } else {
-                            subIndex * (chunkNum + 1)
-                        }
-                        for (baseI in subIndex * chunkNum..end) {
-                            for ((baseJ, row) in matrix[subIndex].withIndex()) {
-                                var neighCount = 0
-                                //val baseI = subIndex
-                                val i = baseI - 1
-                                val j = baseJ - 1
-                                for (x in 0..3) {
-
-                                    if (isOutOfBounds(i + x, height)) {
-                                        continue
-                                    }
-                                    for (y in 0..3) {
-                                        if (i + x == j + y) {
-                                            continue
-                                        }
-                                        if (isOutOfBounds(j + y, width)) {
-                                            continue
-                                        }
-                                        neighCount += matrix[i + x][j + y]
-                                    }
-                                }
-                                //println(neighCount)
-                                newMatrix[baseI][baseJ] = getNewCellState(matrix[baseI][baseJ], neighCount)
-                            }
-                            //}
-                        }
-                    }
-                }
-                matrix = newMatrix.toMutableList()
-                drawPopulation()
-                println("Elapsed" + (System.currentTimeMillis() - start))
-            }while (infinity)
-        }
-    }
-
-    fun runGameOfLife(infinity: Boolean) {
-        //while (true) {
-        //val coroutines = mutableListOf<Job>()
-        var start = System.currentTimeMillis()
-        val index = AtomicInteger(0)
-        val jIndex = AtomicInteger(0)
-        runBlocking {
-            do{
-                start = System.currentTimeMillis()
-                index.set(0)
-            val newMatrix = matrix.toMutableList()
-                coroutineScope {
-                    List(width) {
-
-                        launch {
-                            val subIndex = index.getAndIncrement()
-                            for ((baseJ, row) in matrix[subIndex].withIndex()) {
-                                var neighCount = 0
-                                val baseI = subIndex
-                                val i = baseI - 1
-                                val j = baseJ - 1
-                                for (x in 0..3) {
-
-                                    if (isOutOfBounds(i + x, height)) {
-                                        continue
-                                    }
-                                    for (y in 0..3) {
-                                        if (i + x == j + y) {
-                                            continue
-                                        }
-                                        if (isOutOfBounds(j + y, width)) {
-                                            continue
-                                        }
-                                        neighCount += matrix[i + x][j + y]
-                                    }
-                                }
-                                //println(neighCount)
-                                newMatrix[baseI][baseJ] = getNewCellState(matrix[baseI][baseJ], neighCount)
-                            }
-                        }
-                    }
-                }
-                matrix = newMatrix.toMutableList()
-                drawPopulation()
-                println("Elapsed" + (System.currentTimeMillis()-start))
-            }while (infinity)
-            }
-
-            //println("\u001Bc")
-
-
-        /*runBlocking {
-            val newMatrix = matrix.toMutableList()
-            for ((index, row) in matrix.withIndex()) {
-                //coroutines.add(launch {
-                launch {
-                    for ((jIndex, column) in row.withIndex()) {
-                        var neighCount = 0
-                        var i = index - 1
-                        var j = jIndex - 1
-                        for (x in 0..3) {
-
-                            if (isOutOfBounds(i + x, height)) {
-                                continue
-                            }
-                            for (y in 0..3) {
-                                if (i + x == j + y) {
-                                    continue
-                                }
-                                if (isOutOfBounds(j + y, width)) {
-                                    continue
-                                }
-                                neighCount += matrix[i + x][j + y]
-                            }
-                        }
-                        //println(neighCount)
-                        newMatrix[index][jIndex] = getNewCellState(matrix[index][jIndex], neighCount)
-                }}//)
-            }
-            /*for (coroutine in coroutines){
-                coroutine.join()
-            }*/
-            matrix = newMatrix.toMutableList()
-            //println("\u001Bc")
-            drawPopulation()
-        }*/
-        println("Elapsed" + (System.currentTimeMillis()-start))
-        //}
-
-        /*val newMatrix = matrix.toMutableList()
-        for ((index, row) in matrix.withIndex()) {
-            for ((jIndex, column) in row.withIndex()) {
-                var neighCount = 0
-                var i = index - 1
-                var j = jIndex - 1
-                for (x in 0..3) {
-
-                    if (isOutOfBounds(i + x, height)) {
-                        continue
-                    }
-                    for (y in 0..3) {
-                        if (i + x == j + y) {
-                            continue
-                        }
-                        if (isOutOfBounds(j + y, width)) {
-                            continue
-                        }
-                        neighCount += matrix[i + x][j + y]
-                    }
-                }
-                //println(neighCount)
-                newMatrix[index][jIndex] = getNewCellState(matrix[index][jIndex], neighCount)
-            }
-        }*/
-    }
-    fun drawPopulation(){
+    private fun drawPopulation(){
         if (output != null) {
             for ((i, row) in matrix.withIndex()) {
                 for ((j, col) in row.withIndex()) {
--- src/main/kotlin/gameOfLifeRun.kt
+++ src/main/kotlin/gameOfLifeRun.kt
@@ -5,28 +5,24 @@ import javafx.scene.control.Button
 import javafx.scene.layout.AnchorPane
 import javafx.scene.layout.Pane
 import javafx.scene.paint.Color
-import javafx.scene.paint.Paint
 import javafx.stage.Stage
 import javafx.scene.shape.Rectangle
 import javafx.animation.Animation
 import javafx.event.ActionEvent
 import javafx.event.EventHandler
-import java.time.Clock.tick
-import javafx.util.Duration.millis
 import javafx.animation.KeyFrame
 import javafx.animation.Timeline
 import javafx.util.Duration
 
 
 fun main(args: Array<String>) {
-    //Application.launch(Main::class.java,*args)
     val size = 5000
     val gameOfLife = GameOfLife(size,size,null)
     gameOfLife.generateMatrix()
     gameOfLife.runGameOfLifeThreads(true)
 }
 
-class Main() : Application() {
+class Main : Application() {
 
     var gameOfLife: GameOfLife? = null
     var action: Timeline? = null
@@ -37,11 +33,10 @@ class Main() : Application() {
         val gridY = size
         val recSize = 10.0
         val grid = mutableListOf<MutableList<Rectangle>>()
-        var isRunning = false
 
         val basePane = AnchorPane()
         val btnStart = Button("Start game")
-        basePane.children.add(btnStart);
+        basePane.children.add(btnStart)
         AnchorPane.setTopAnchor(btnStart, 1.0)
         AnchorPane.setLeftAnchor(btnStart, 1.0)
         AnchorPane.setRightAnchor(btnStart, 1.0)
@@ -50,11 +45,11 @@ class Main() : Application() {
 
             override fun handle(event: ActionEvent) {
                 if (action?.status == Animation.Status.RUNNING) {
-                    action?.stop();
-                    btnStart.setText("Start game");
+                    action?.stop()
+                    btnStart.text = "Start game"
                 }else {
-                    action?.play();
-                    btnStart.setText("Stop game");
+                    action?.play()
+                    btnStart.text = "Stop game"
                 }
             }
         }
@@ -72,10 +67,7 @@ class Main() : Application() {
             grid.add(mutableListOf())
             for (y in 0..gridY){
                 val rectangle = Rectangle(recSize,recSize)
-                //rectangle.setStroke(Color.WHITE)
                 rectangle.strokeWidth = 0.0
-                /*rectangle.x = recSize * x
-                rectangle.y = recSize * y*/
                 rectangle.fill = Color.RED
                 rectangle.xProperty().bind(rectSize.multiply(x))
                 rectangle.yProperty().bind(rectSize.multiply(y))
@@ -88,45 +80,21 @@ class Main() : Application() {
         val scene = Scene(basePane, 800.0,600.0)
 
         action = Timeline(KeyFrame(Duration.millis(100.0), object : EventHandler<ActionEvent> {
-            private val counter: Int = 0 //Canvas, Image, Timer
             override fun handle(event: ActionEvent) {
-                /*counter++;
-                Point end = snake.get(snake.size()-1);
-                Point next = new Point((end.x+direction.x+grid_x)%grid_x,(end.y+direction.y+grid_y)%grid_y);
-                snake.add(next);
-                grid[next.x][next.y].setFill(Color.RED);
-                if(counter%3==0){
-                    counter=0;
-                    Point start = snake.get(0);
-                    grid[start.x][start.y].setFill(Color.BLACK);
-                    snake.remove(0);
-                }*/
-
-                /*grid[point.x][point.y].setFill(Color.BLACK);
-                point.x = (point.x+direction.x+grid_x)%grid_x;
-                point.y = (point.y+direction.y+grid_y)%grid_y;
-                grid[point.x][point.y].setFill(Color.RED);*/
                 gameOfLife?.runGameOfLifeThreads(false)
             }
         }))
 
-        action?.cycleCount = Timeline.INDEFINITE;
+        action?.cycleCount = Timeline.INDEFINITE
 
         primaryStage?.scene = scene
         primaryStage?.show()
         gameOfLife = GameOfLife(gridX,gridY,grid)
         gameOfLife?.generateMatrix()
 
-        /**/
     }
 
 
 
-    /*companion object {
-        @JvmStatic
-        fun main() {
-            launch(Main::class.java)
-        }
-    }*/
 
 }
